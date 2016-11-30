e8ad4505a9e7145160ff775700dcae4f52fbac6f
DaltonMcc <mccleeryd17@hanover.edu>
2016-11-29 19:15:27 -0500

    Clean Signup process. Ref #17
    Separate HTML formating from PHP logic.

--- signup.php
+++ signup.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-  <head>
-
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="css/style.css" />
-
-  </head>
-  <body>
-
 <?php
-require('dbconnect.php');
+require 'dbconnect.php';
+include 'signup_screen.html';
 
 // If form submitted, insert values into the database.
 if (isset($_POST['username'])){
@@ -24,27 +14,6 @@ if (isset($_POST['username'])){
         $result = mysqli_query($conn, $query);
         //echo ($result);
         if($result){
-            echo " <h3>You are registered successfully!</h3>
-                   <br/>Click here to <a href='login.php'>Login</a>";
-        }
-    }else{
+            header("Location: login.php");
+        }}
 ?>
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Sign Up!</b></div>
-                
-            <div style = "margin:30px">
-               
-               <form action = "" method = "POST">
-                  <label>Username  :</label><input type = "text" name = "username" class = "box" required /><br>
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" required /><br>
-                  <label>Name  :</label><input type = "fname" name = "fname" class = "box" required /><br>
-                  <input type = "submit" name="submit" value = "Register"/><br>
-                  <p>Already registered? <a href='login.php'>Login Here</a></p>
-               </form>
-            </div>
-          </div>
-      </div>
-<?php } ?>
-  </body>
-</html>
