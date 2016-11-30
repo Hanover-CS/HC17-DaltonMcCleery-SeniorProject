e3585b70aa14774ba1c8692db4f234ee5465490d
DaltonMcc <mccleeryd17@hanover.edu>
2016-11-29 20:36:27 -0500

    Fixed Function Querying Issue. Close #19.
    Needed an extra variable, in this case the database connection, within each function call. Without this, I could not process a query inside a function because it could not access my original connection variable that is declared in dbconnect.

--- functions.php
+++ functions.php
@@ -1,25 +1,43 @@
 <?php
-	include_once 'dbconnect.php';
+	require_once 'dbconnect.php';
 
 	//get the file address from the library tables
-	function get_file_address($id, $table)
+	function get_file_address($id, $table, $conn)
 	{
-		$query = "SELECT file FROM '$table' WHERE id = '$id'";
+		$query = "SELECT file FROM $table WHERE id = '$id'";
+		//echo $query;
 		$result = mysqli_fetch_array( mysqli_query($conn, $query) );
-		return $result['file'];
+		return $result[0];
 		//return $query;
 	}
 
+
 	//get the user's ID by using their username and password
-	function get_id($user, $pass)
+	function get_id($user, $pass, $conn)
 	{
    		$get_id = "SELECT id FROM chops_students WHERE username= '$user' and password= '$pass'";
 
-   		//execute then convert the get_id query into an array
-   		$res = mysqli_fetch_array(mysqli_query($conn, $get_id));
+   		//execute then convert the get query into an array
+   		$result = mysqli_fetch_array(mysqli_query($conn, $get_id));
    
    		//then access the 'id' field of that array from the query
-   		return $res['id'];
+   		return $result['id'];
 	}
+
+
+	//get person's name
+	function get_fname($user, $conn)
+	{
+		$get_name = "SELECT name FROM chops_students WHERE username= '$user'";
+
+		 //execute then convert the get query into an array
+   		$result = mysqli_fetch_array(mysqli_query($conn, $get_id));
+   
+   		//then access the 'id' field of that array from the query
+   		return $result['name'];
+
+	}
+
+	//more functions go here...
 
 ?>
