c34e5c443e54a064e1a5b736036204c1230bc6f3
DaltonMcc <mccleeryd17@hanover.edu>
2016-11-29 19:02:56 -0500

    Query Troubles. Ref #19
    Some test files I'm working with to display images that I query from the database. test3.php works fine, but test4.php gives me an error: "Undefined variable '$conn" on line 8 of functions.php." Need to fix this.

---
+++ test4.php
@@ -0,0 +1,14 @@
+<?php
+	include 'functions.php';
+?>
+
+<html>
+<head>
+
+		<?= get_file_address("1", "chops_etudes"); ?>
+
+
+  		<img src="<?= get_file_address(1, 'chops_etudes'); ?>" alt="Etude" style="width:242px; height:200px;">
+
+</head>
+</html>
