13d07f63846d9972607fe03ac02a57bb212be426
DaltonMcc <mccleeryd17@hanover.edu>
2017-01-18 15:54:06 -0500

    Started outlines and basic functions of Rudiments. Ref #14
    Basic "homepage" for rudiments to be housed along with links to go into individual rudiment pages (that have not been implemented yet)

---
+++ rudiment.php
@@ -0,0 +1,9 @@
+<?php
+  	include 'navbar.php';
+
+  	//determines which rudiment to display
+	$rudi = $_GET['id'];
+  	$ID = get_id($_SESSION['username'], $_SESSION['password'], $conn);
+
+  	html_starter();
+?>
