b5900671bef8253c8c03bdf7d1709e386ca65596
DaltonMcc <mccleeryd17@hanover.edu>
2017-02-08 20:06:39 -0500

    Begin fixing 'Include' paths. Ref #22
    Fix the include/require file paths for the main two files. These need to be able to correctly display content or get content from a file within a different directory or folder.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 	include 'navbar.php';
 
 	//include 'something else stylish.html';
--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <?php
 
   session_start();
-  include 'functions.php';
-  require 'dbconnect.php';
+
+  include $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-chops/Functions/functions.php';
+  require $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-chops/Database/dbconnect.php';
 
   $person = $_SESSION['fname'];
-  //echo $person;
 
 ?>
 
@@ -26,20 +26,20 @@
 
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <a class="navbar-brand" href="index.php">Chops</a>
+        <a class="navbar-brand" href="/chops/hc07-chops/index.php">Chops</a>
       </div>
 
       <!-- navbar Links to go to the different libraries -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
 
-          <li><a href="library.php?table=chops_etudes">Etudes</a></li>
+          <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_etudes">Etudes</a></li>
            
-          <li><a href="library.php?table=chops_videos">Videos</a></li>
+          <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_videos">Videos</a></li>
 
-          <li><a href="library.php?table=chops_audio">Audio</a></li>
+          <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_audio">Audio</a></li>
 
-          <li><a href="chops_rudiments.php">Rudiments</a></li>
+          <li><a href="/chops/hc07-chops/Library/chops_rudiments.php">Rudiments</a></li>
 
         </ul>
 
@@ -55,8 +55,8 @@
       </ul>
 
       <ul class="nav navbar-nav navbar-right">
-		    <p class="navbar-text navbar-right"><a href="logout.php" class="navbar-link"> Sign out </a></p> 
-		    <p class="navbar-text navbar-right">Signed in as <a href="update_user.php" class="navbar-link"> <?= $person ?></a></p>
+		    <p class="navbar-text navbar-right"><a href="/chops/hc07-chops/Login/logout.php" class="navbar-link"> Sign out </a></p> 
+		    <p class="navbar-text navbar-right">Signed in as <a href="/chops/hc07-chops/Login/update_user.php" class="navbar-link"> <?= $person ?></a></p>
       </ul>
 
     </div><!-- /.navbar-collapse -->
