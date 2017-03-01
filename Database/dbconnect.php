262ef97fc6cf9b76ead1573ab598efae137c8a11
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-01 14:00:19 -0500

    Work on Database Class. Close #26
    Implement a construct method that initializes the connection, method for finding One item, method for finding One Row of data, method for finding multiple rows of data, and a method for testing a database query.

--- Database/dbconnect.php
+++ Database/dbconnect.php
@@ -1,31 +1,84 @@
 <?php
- 
 
- 
-//include the setup file with all the connection variables
- 
+	class Database {
 
- 
-  //require_once 'dummy_server.php';
- 
-  require_once 'parameters.php'; //<-- for local WAMP server
- 
+    public $conn,
 
- 
-//connect to database
- 
-  $conn = mysqli_connect($server[0], $server[1], $server[2], $server[3]);
- 
+    //$server variable should be an ARRAY with
+    //the first element being the server host,
+    //the second element being the username of the database,
+    //the third being the password to the database,
+    //the last (fourth) being the server name.
+    function __construct($server) 
+    {
+      $this->conn = mysqli_connect($server[0], $server[1], $server[2], $server[3]);
 
- 
-// Error should be NULL or "false" is connection was successful. 
- 
-// Otherwise, the error is not NULL and the statment is "true," thus killing the script
- 
-  if (mysqli_connect_error())
- 
-    { die ("Error connecting to Database."); }
- 
+      if (mysqli_connect_error()) 
+      { 
+        die ("Error connecting to Database. Check Database variables.");
+      }
+    }
 
+    //This method returns only ONE item from the Database
+    //using the $ID as an identifer/row,
+    //the $table as to which table to access,
+    //and the $column to pinpoint the exact item to grab.
+    function findOne($ID, $table, $column)
+    {
+      $query = "SELECT $column FROM $table WHERE id = $ID";
  
+      $result = mysqli_fetch_array( mysqli_query($this->conn, $query) );
+
+      testForError($result);
+ 
+      return $result[0];
+    }
+
+    //Same as findOne method except it returns a row of information instead of 1 item.
+    function findOneRow($ID, $table)
+    {
+      $query = "SELECT * FROM $table WHERE id = $ID";
+ 
+      $result = mysqli_fetch_array( mysqli_query($this->conn, $query) );
+
+      testForError($result);
+ 
+      return $result[0];
+    }
+
+    //Returns an array of data from the Database.
+    //Rows are stored one at a time in the While loop
+    function findMany($table, $column)
+    {
+      $results = [];
+      $counter = 0;
+
+      $query = "SELECT $column FROM $table";
+
+      $result = mysqli_query($this->conn, $query);
+
+      testForError($result);
+
+      while ($row = mysqli_fetch_assoc($result)) 
+      {
+        $results[$counter] = $row;
+        $counter = $counter + 1;
+      }
+
+      return $results;
+    }
+
+    //This protected method is used to test if there was
+    //an error performing a Database query.
+    protected function testForError($result)
+    {
+      if (!$result)
+      {
+        die ("Error retreiving information from Database."); 
+      }
+    }
+
+
+  }
+
 ?>
