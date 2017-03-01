5a72184795b5e2b972b5faa8005be11f813e6464
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-01 13:55:44 -0500

    Clean student_class file
    Adjust spacing/formatting

--- Functions/student_class.php
+++ Functions/student_class.php
@@ -1,6 +1,8 @@
 <?php
+
+	include 'functions.php';
 	
-	class student {
+	class Student {
 
 		//class properities
 		public $username;
@@ -9,41 +11,60 @@
 		protected $user_id;
 
 		//constructor used to initialize a student
-		function __construct($student_name, $student_username, $student_password) {
-
+		function __construct($student_name, $student_username, $student_password) 
+		{
 			$this->name = $student_name;
 			$this->username = $student_username;	
 			$this->password = $student_password;
-
 		}
 
 		//class methods
 		protected function get_user_id()
-			{ return $this->user_id; }
+		{
+			return $this->user_id;
+		}
 
 
 		function set_name($new_name) 
-			{ $this->name = $new_name; }
+		{
+			$this->name = $new_name;
+		}
 
 
 		function get_name() 
-			{ return $this->name; }
+		{
+			return $this->name;
+		}
 
 
 		function set_username($new_username) 
-			{ $this->username = $new_username; }
+		{
+			$this->username = $new_username;
+		}
 
 
 		function get_username() 
-			{ return $this->username; }
+		{
+			return $this->username;
+		}
 
 
 		private function set_password($new_password)
-			{ $this->password = $new_password; }
+		{
+			$this->password = $new_password;
+		}
 
 
 		private function get_password()
-			{ return $this->password; }
+		{
+			return $this->password;
+		}
+
+
+		function getFavorites()
+		{
+
+		}
 
 	}
 
