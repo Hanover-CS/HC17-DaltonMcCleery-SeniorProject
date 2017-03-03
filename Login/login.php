7c703ced7b97165039d68126ff01fd29102d1e0a
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-02 23:37:09 -0500

    Fix login system to use the Class system. Ref #32
    Instead of having the file do all the work with the queries, I offload those processes to the DB Class that handles all query work. I create a temporary Student object (with the information the user entered in the login forum) as a checker against the information I have in the Database. If the two match, the user is logged in; setting the SESSION variables so the main functions file can build the real Student Object.

--- Login/login.php
+++ Login/login.php
@@ -2,8 +2,9 @@
 	session_start();
 
 	//connect to database
- 	require $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-Chops/Database/dbconnect.php';
-  	include $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-Chops/Functions/functions.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-Chops/Database/dbconnect.php';
+	use \chops\hc07chops\Database\dbconnect;
+	include $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-Chops/Functions/student_class.php';
 	
 	require_once 'log_screen.html';
 
@@ -11,43 +12,23 @@
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	//get and store user creds from the forum
-	$username = mysqli_real_escape_string($conn, $_POST['username']);
-	$password = mysqli_real_escape_string($conn, $_POST['password']);
-	$query = "SELECT * FROM chops_students WHERE username = '$username' and password = '$password'";
+	$username = mysqli_real_escape_string(Database::connect()->conn, $_POST['username']);
+	$password = mysqli_real_escape_string(Database::connect()->conn, $_POST['password']);
+	
+	$temp_student = new Student($username, $password);
 
-	$result = mysqli_query($conn, $query);
- 	$count = mysqli_num_rows($result);
+	if ( $temp_student->checkLogin($username, $password) )
+	{
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password;
+		$_SESSION['fname'] = $temp_student->getName();
 
-	//check to see if query was successful
-	if ($result) {
+		//if everything matches correctly, take them to the homepage
+		header("Location: /chops/hc07-chops/index.php");
+	} else {
 
-		$store_res = mysqli_fetch_array($result);
-		//check to see it returned only 1 row
-		if ($count == 1) {
-
-			if ($username != $store_res['username'])
-				{ echo "<div class='alert alert-warning' role='alert'> FAKE </div>"; }
-
-
-			//$student = new Student($store_res['fname'], $username, $password);
-			//$_SESSION['student'] = new Student($store_res['fname'], $username, $password);
-
-			$_SESSION['username'] = $username;
-			$_SESSION['password'] = $password;
-			$_SESSION['fname'] = $store_res['fname'];
-
-
-			//if everything matches correctly, take them to the homepage
-			header("Location: ./../index.php");
-						
-			} else {
-				//if they dont, throw an error
-				die ("<div class='alert alert-warning' role='alert'> Incorrect Login! </div>");
-				} 
-		
-		} else {
-			//if they dont, throw an error
-			die ("<div class='alert alert-warning' role='alert'> Cannot Find User! </div>");
+		//if they dont, throw an error
+		die ("<div class='alert alert-warning' role='alert'> Incorrect Login! </div>");
 		} 
 
 }?>
