0fb79e52656c14c3eeb105894427d28b36cff250
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-02 23:08:13 -0500

    Update Student Class. Close #29
    Revamp some structure, paticularly in the constructor and in the Favorites methods. Using te DB Class to perform the queries. Also, update method names to more conventional naming practices.

--- Functions/student_class.php
+++ Functions/student_class.php
@@ -1,6 +1,6 @@
 <?php
 
-	include 'functions.php';
+	use \chops\hc07chops\Database\dbconnect;
 	
 	class Student {
 
@@ -11,64 +11,65 @@
 		protected $user_id;
 
 		//constructor used to initialize a student
-		function __construct($student_name, $student_username, $student_password) 
+		function __construct($student_username, $student_password) 
 		{
-			$this->name = $student_name;
 			$this->username = $student_username;	
 			$this->password = $student_password;
+			$this->user_id = Database::connect()->findOneWithoutID("username", $student_username, 
+											"password", $student_password, "chops_students", "id");
+			$this->name = Database::connect()->findOne($this->user_id, "chops_students", "fname");
 		}
 
 		//class methods
-		protected function get_user_id()
+		function getUserID()
 		{
 			return $this->user_id;
 		}
 
 
-		function set_name($new_name) 
+		function setName($new_name) 
 		{
 			$this->name = $new_name;
 		}
 
 
-		function get_name() 
+		function getName() 
 		{
 			return $this->name;
 		}
 
 
-		function set_username($new_username) 
+		function setUsername($new_username) 
 		{
 			$this->username = $new_username;
 		}
 
 
-		function get_username() 
+		function getUsername() 
 		{
 			return $this->username;
 		}
 
 
-		private function set_password($new_password)
+		function setPassword($new_password)
 		{
 			$this->password = $new_password;
 		}
 
 
-		private function get_password()
+		function getPassword()
 		{
 			return $this->password;
 		}
 
 		// --- FAvORITES Section --- //
+		// ---  ---  TODO  ---  --- //
 
 		//Returns true if a file IS favorited by a student.
 		//Returns false if it is not. 
 		function checkFavorite($file)
 		{
-			$query = "SELECT $file FROM chops_favorites WHERE student_id = $this->student_id";
-
-			$result = mysqli_fetch_array( mysqli_query($Database->getConn(), $query) );
+			$result = Database::connect()->findOne($this->user_id, 'chops_favorites', $file);
 
 			if ($result)
 			{
@@ -84,7 +85,30 @@
 		{
 			$values = [$this->user_id, $Content->get_file_id(), $Content->get_file_address()];
 
-			$Database->addToFavorites($values);
+			Database::connect()->addToFavorite($values);
+		}
+
+		//TODO
+		function removeFavorite($Content)
+		{
+			$values = [$this->user_id, $Content->get_file_id(), $Content->get_file_address()];
+
+			Database::connect()->removeFromFavorite($values);
+		}
+
+		//TODO
+		function checkLogin($username, $password)
+		{
+			$dbuser_id = Database::connect()->findOneWithoutID("username", $username, 
+													"password", $password, "chops_students", "id");
+			$dbuser = Database::connect()->findOneRow($dbuser_id, "chops_students");
+
+			if ($dbuser['username'] != $username && $dbuser['password'] != $password)
+			{
+				return false;
+			}
+
+			return true;
 		}
 
 	}
