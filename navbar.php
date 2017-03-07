be1b4dd7d36872aa5f0f38d7dee4f6fc131d6214
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-06 22:11:29 -0500

    Updated Navbar
    Included a dropdown menu in the Navbar for better navigation. Also, changed the name of About.php to help.php, better name change because I want to include some tutorial-like hints in help.php to help users better understand some of the functionality.

--- navbar.php
+++ navbar.php
@@ -12,6 +12,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<!-- Bootstrap -->
     	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+      <!-- Extra files to make the Dropdown menu work correctly -->
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+      <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
   </head>
 
   <body>
@@ -28,16 +33,21 @@
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
 
+          <!-- Chops Etude Library Page -->
           <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_etudes">Etudes</a></li>
-           
+          
+          <!-- Chops Video Library Page -->
           <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_videos">Videos</a></li>
 
+          <!-- Chops Audio Library Page -->
           <li><a href="/chops/hc07-chops/Library/library_builder.php?table=chops_audio">Audio</a></li>
 
+          <!-- Chops Rudiments Homepage -->
           <li><a href="/chops/hc07-chops/Library/chops_rudiments.php">Rudiments</a></li>
 
         </ul>
 
+      <!-- Search through the available Etudes, Audio, and Video -->
       <form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
@@ -45,15 +55,34 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
 
-      <ul class="nav navbar-nav">
-      <li><a href="/chops/hc07-chops/about.php">About</a></li>
+      <!-- Dropdown Menu for Student's organizational links -->
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown" class="navbar-text navbar-right">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $student->getName() ?><span class="caret"></span></a>
+
+          <ul class="dropdown-menu">
+
+            <!-- Link to go to the Update User page where a Student can
+            change their username, password, or name -->
+            <li><a href="/chops/hc07-chops/Login/update_user.php">Update Info</a></li>
+
+            <!-- Link to go to the Helpful hint page to learn about some of the Features of Chops -->
+            <li><a href="/chops/hc07-chops/help.php">Help</a></li>
+
+            <!-- Work in Progress/TBA -->
+            <li><a class="btn disabled" href="#">Chat/Forums</a></li>
+
+            <!-- Link to Destroy the Session variables and Sign the Student out.
+            Takes them back to the login page -->
+            <li role="separator" class="divider"></li>
+            <li><a href="/chops/hc07-chops/Login/logout.php">Sign Out</a></li>
+          </ul>
+        </li>
       </ul>
 
-      <ul class="nav navbar-nav navbar-right">
-		    <p class="navbar-text navbar-right"><a href="/chops/hc07-chops/Login/logout.php" class="navbar-link"> Sign out </a></p> 
-		    <p class="navbar-text navbar-right">Signed in as <a href="/chops/hc07-chops/Login/update_user.php" class="navbar-link"> 
-        <?php echo $student->getName() ?></a></p>
-      </ul>
+      <!-- This must come after the Dropdown menu above, due to spacing and layout
+      with the "navbar-right" formatting -->
+      <p class="navbar-text navbar-right">Signed in as</p>
 
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
