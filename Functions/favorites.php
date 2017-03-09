13e8a5b3a1c754db4d21ab4d14a58bfb3dff8c11
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-09 15:56:33 -0500

    Change file name of Favorites page
    Changed name from "add_favorites.php" to just "favorites.php" because I want this page to handle both Favoriting some content AND unFavoriting it as well.

---
+++ Functions/favorites.php
@@ -0,0 +1,37 @@
+<?php
+
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-chops/Functions/functions.php';
+
+  //the HTML form from the Favorite's button from the Twig Template file 
+  //already has it's inputs populated with the correct info about what file is being added and from what table. 
+  //The info is "hidden" from the user but is accessable through the address bar.
+  //So when a student clicks the Favorite! button of a file, it runs the HTML form
+  //and that throws the file and table info into the URL and loads this file. 
+  //This file grabs that info and puts it into another function call that then adds the file into the database.
+
+  $table = $_GET['table'];
+  $fileID = $_GET['fileID'];
+  $status = $_GET['status'];
+
+  //use the GET variables pass to us by the URL to temporarly create a new Content Object
+  $content = new Content($fileID, $table);
+
+  if ($status == 'add')
+  {
+    //Call the addFavorite method of the Student Class,
+    //using are new Content Object, to add it to the Student's Favorites.
+    $student->addFavorite($content);
+
+    //if successfull, this will take the user back to the Libray they were in
+    //and updates the Favorite Button to display "Favorited!" with a gold star.
+    header("Location: /chops/hc07-chops/Library/library_builder.php?table=" . $table);
+
+  } else if ($status == 'remove')
+  {
+    $student->removeFavorite($content);
+    //if successfull, this will take the user back to their homepage.
+    //Updates the file's Favorite Button to display "Favorite!" with an empty star
+    header("Location: /chops/hc07-chops/index.php");
+  }
+
+?>
