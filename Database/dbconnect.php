5ec43a33dd8814273c2e3d59df1a000d13901951
DaltonMcc <mccleeryd17@hanover.edu>
2017-03-25 23:39:24 -0400

    Writing documentation for Database Class. Ref #37
    Using phpDoc's syntax and structure, create documentation for each Database method within the dbconnect file

--- Database/dbconnect.php
+++ Database/dbconnect.php
@@ -1,4 +1,7 @@
 <?php
+  /**
+   * This is the main file that interacts with the data in the tables in the Database.
+   */
 	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-chops/Database/dummy_server.php';
   // require_once $_SERVER['DOCUMENT_ROOT'] . '/chops/hc07-chops/Database/parameters.php';
@@ -24,12 +27,16 @@
       }
     }
 
-    //This is the main method of the DB Class
-    //When this method is called (and it can be called statically without referencing an object),
-    //it checks an instance property, above, to see if there was a DB connection previously made.
-    //If there was, then there is no need to create another connection, just return the first (and only) connection.
-    //If there was not, then it (and only it) calls the construct method of this class
-    //and creates a DB connection.
+    /**
+     * This is the main method of the DB Class.
+     *
+     * When this method is called (and it can be called statically without referencing an object),
+     * it checks an instance property, above, to see if there was a DB connection previously made.
+     * If there was, then there is no need to create another connection, just return the first (and only) connection.
+     * If there was not, then it (and only it) calls the construct method of this class and creates a DB connection.
+     *
+     * @return the mysqli connection object
+     */
     public static function connect()
     {
     	if (self::$instance == null)
@@ -40,20 +47,32 @@
     	return self::$instance;
     }
 
-    //set Server info for Database connection. 
-  	//The Dummy_server (Parameters) file gives us an Array of DB variables used to
-  	//connection to the Database.
-  	//Array should be formatted as: [$dbhost, $dbuser, $password, $dbname]
+    /**
+     * set Server info for Database connection. 
+  	 *
+     * The Dummy_server (Parameters) file gives us an Array of DB variables used to
+     * connection to the Database.
+     *
+  	 * @param array $server_array should be formatted as: [$dbhost, $dbuser, $password, $dbname]
+     *
+     * @return the information provided
+     */
     static function setServer($server_array)
     {
     	self::$serverinfo = $server_array;
     	return self::$serverinfo;
     }
 
-    //This method returns only ONE item from the Database
-    //using the $ID as an identifer/row,
-    //the $table as to which table to access,
-    //and the $column to pinpoint the exact item to grab.
+    /**
+     * This method returns only ONE item from the Database.
+     *
+     * Using the $ID as an identifer/row, the $table as to which table to access,
+     * and the $column to pinpoint the exact item to grab.
+     *
+     * @param tuple with an ID identifier, which table to grab from, and what column is wanted
+     *
+     * @return one item of information from a certain table in the Database
+     */
     function findOne($ID, $table, $column)
     {
       $query = "SELECT $column FROM $table 
@@ -66,9 +85,16 @@
       return $result[$column];
     }
 
-    //The idea here is to access some DB information without using the ID field.
-    //Some instances arise where the ID is unknown but other pieces of information are known.
-    //I safe guarded some of this by having to know at least 2 other pieces/fields to get at the info
+    /**
+     * Access some DB information without using the ID field.
+     *
+     * Some instances arise where the ID is unknown but other pieces of information are known.
+     * I safe guarded some of this by having to know at least 2 other pieces/fields to get at the info
+     *
+     * @param tuple with 2 sets of variables for the WHERE clause, and a column and table to access
+     *
+     * @return one item of information from a certain table in the Database
+     */
     function findOneWithoutID($fieldname1, $fieldvar1, $fieldname2, $fieldvar2, $table, $column)
     {
       $query = "SELECT $column FROM $table
@@ -82,8 +108,15 @@
       return $result[$column];
     }
 
-    //Same as findOne method except it returns a row of information instead of 1 item.
-    //Ex. findOne returns something like 'etude_name'
+    /**
+     * Same as findOne method except it returns a row of information instead of 1 item.
+     * 
+     * Grabs an entire row of information, instead of just one item within a row.
+     *
+     * @param tuple with an ID identifier and a table with which to access from
+     *
+     * @return array with index's that match the column data in the table that was given
+     */
     //findOneRow returns ['etude_name', 'etude_ID', 'etude_composer', 'etude_file']
     function findOneRow($ID, $table)
     {
@@ -97,8 +130,17 @@
       return $result;
     }
 
-    //Returns an array of data from the Database.
-    //Rows are stored one at a time in the While loop
+    /**
+     * Grabs multiple rows of data
+     *
+     * Similar to the findOneRow method, except the where clause is more open-ended
+     * and might include more than one row of information. Rows are stored using a simple
+     * for-loop process.
+     *
+     * @param tuple with an ID and table (similar to others) and a explicitly expressed where clause
+     *
+     * @return array
+     */
     function findMany($table, $column, $whereclause, $equals)
     {
       $results = [];
@@ -120,7 +162,13 @@
       return $results;
     }
 
-    //Counts how many rows there are in a given Table
+    /**
+     * Simple count method to see how many rows there are for a table in the database
+     *
+     * @param a table name in the database
+     *
+     * @return integer
+     */
     function countRows($table)
     {
     	$query = "SELECT 'COUNT(*)' FROM $table";
@@ -132,7 +180,16 @@
       return $result;
     }
 
-    //Query method used to add a file to the Favorites table based on the current student's ID
+    /**
+     * Query method used to add a file to the Favorites table based on the current student's ID
+     *
+     * This method inserts a new entry into the chops_favorites table and fills in the columns
+     * with the current Student's ID, the file's address, table, and ID.
+     *
+     * @param array that is formatted as: [student_id, file's ID, file's address, and the file's origin table name]
+     *
+     * @return void
+     */
     function addToFavorite($values)
     {
     	$query = "INSERT INTO chops_favorites (student_id, file_id, file, lib_table) 
@@ -145,7 +202,16 @@
 			// return true;
     }
 
-    //Query method used to remove a file from the Favorites table based on the current student's ID
+    /**
+     * Query method used to remove a file from the Favorites table based on the current student's ID
+     *
+     * very similar to the addToFavorites method except it is deleting a row in the chops_favorites
+     * table that has the corrosponding values
+     *
+     * @param array that is formmatted as: [student_id, file's ID, and the file's origin table name]
+     *
+     * @return boolean if successful
+     */
     function removeFromFavorite($values)
     {
       $query = "DELETE FROM chops_favorites 
@@ -160,6 +226,16 @@
 			return true;
     }
 
+    /**
+     * Check method to see if a Row exists in the Student Progress table
+     *
+     * Given a Student's ID and a specific file's address, checks to see if
+     * a row in the chops_favorites table has the matching student_id and file address fields
+     *
+     * @param tuple with a student's ID and a file's address
+     *
+     * @return boolean if successful
+     */
     function checkStudentFavorite($studentID, $file)
     {
       $studentFavorites = $this->getStudentFavorites($studentID);
@@ -177,6 +253,16 @@
       return false;
     }
 
+    /**
+     * Gets all rows corrosponding to the current Student's ID
+     *
+     * Using the logged in Student's ID, make an array with all the information
+     * that is associated with that ID in the chops_favorites table
+     *
+     * @param integer #studentID that is the currently logged in student's ID identifier
+     *
+     * @return array
+     */
     function getStudentFavorites($studentID)
     {
       $results = [];
@@ -197,8 +283,16 @@
       return $results;
     }
 
-    //Query method used to update the information in the Student table of the Database.
-    //$values argument should be structured like: ['username', 'password', 'name', 'ID']
+    /**
+     * Query method used to update the information in the Student table of the Database.
+     *
+     * When a Student updates their Student Object via the update_user form,
+     * it also needs to update the information in the Database
+     *
+     * @param array $values structured like: [student's username, password, fname, and thier ID]
+     *
+     * @return boolean if successful
+     */
     function updateStudentQuery($values)
     {
     	$query = "UPDATE chops_students 
@@ -212,10 +306,21 @@
    		return true;
     }
 
-    //Query method used to insert a new Student into the Student table of the Database.
-    //$values argument should be structured like: ['username', 'password', 'name']
+    /**
+     * Query method used to insert a new Student into the Student table of the Database.
+     *
+     * When a new student creates an account, there must be a new entry into the
+     * chops_students table with the fields filled in with the information they submitted
+     *
+     * @param array $values structured like: [student's username, password, and fname]
+     *
+     * @return boolean if successful
+     */
     function insertStudent($values)
     {
+      
+      //First, check to see if there is a Student already there with the same creds
+
     	$query = "INSERT INTO chops_students (username, password, fname)
                 VALUES ('$values[0]', '$values[1]', '$values[2]')";
 
@@ -227,7 +332,16 @@
    		return true;
     }
 
-    //TODO
+    /**
+     * Add new "Progress" row in the Student Progress table
+     *
+     * Insert a new row in the progress table with the current student's ID
+     * and the rudiment's ID with a status field set to 1 for completed and 0 for uncompleted
+     *
+     * @param tuple with the Student's ID and the rudiment's ID
+     *
+     * @return boolean if successful
+     */
     function updateStudentProgress($ID, $r_id)
     {
       $query = "INSERT INTO chops_student_progress (r_id, student_id, status)
@@ -240,7 +354,17 @@
       return true;
     }
 
-    //TODO
+    /**
+     * Check to see if a Row exists in the Student Progress table
+     *
+     * Similar to updateStudentProgress, except this first checks to see if a row
+     * is even there, if it is not, then it is Null and if it is there, it matches the
+     * student ID and rudiment ID with the given variables
+     *
+     * @param tuple with the Student's ID and the rudiment's ID
+     *
+     * @return boolean on success and Null on unsuccessful
+     */
     function checkStudentProgress($ID, $r_id)
     {
       $query = "SELECT status FROM chops_student_progress
@@ -258,6 +382,17 @@
       }
     }
 
+    /**
+     * Method for Deleting a Student and all their related progress
+     *
+     * This multi-step process first deletes all the rows in the Student Progress table
+     * with the associated student ID, then it deletes all the rows in the Favorites table
+     * just like the Progress table, and lastly it deletes the row in the Students table itself
+     *
+     * @param integer $ID as the currently logged in Student's ID
+     *
+     * @return
+     */
     //Query method used to delete a Student user from the Student table in the Database.
     /* protected? */function deleteStudent($ID)
     {
@@ -288,8 +423,13 @@
       return true;
     }
 
-    //This private method is used to test if there was
-    //an error performing a Database query.
+    /**
+     * This private method is used to test if there was an error performing a Database query.
+     *
+     * @param mysqli query result object
+     *
+     * @return throw error on failure
+     */
     private function testForError($result)
     {
       if (!$result)
